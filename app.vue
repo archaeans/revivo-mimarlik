c09e8d871d435a78cdcc72e13654397e7f7a3298
oguzhankoral <oguzhankoral@gmail.com>
2025-03-01 23:32:35 +0300

    typo

--- app.vue
+++ app.vue
@@ -2,6 +2,6 @@
   <div
     class="bg-gradient-to-r from-blue-500 via-blue-400 to-blue-600 inline-block py-1 text-transparent bg-clip-text text-2xl"
   >
-    {{ "Rivera Mimarlık, çok yakında :)" }}
+    {{ "Revivo Mimarlık, çok yakında :)" }}
   </div>
 </template>
