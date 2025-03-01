ae0742ef9da74ec94e99bd6e5d8592957f212d06
oguzhankoral <oguzhankoral@gmail.com>
2025-03-01 22:48:47 +0300

    add tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div
+    class="bg-gradient-to-r from-blue-500 via-blue-400 to-blue-600 inline-block py-1 text-transparent bg-clip-text text-2xl"
+  >
+    {{ "Rivera Mimarlık, çok yakında :)" }}
   </div>
 </template>
